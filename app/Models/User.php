f2e2aa216767f8fa57f47490d5db186c07e795b6
Ali Mohammadi <154596621+Alixx24@users.noreply.github.com>
2024-06-19 01:57:17 +0330

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -34,7 +34,9 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
+ protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
     /**
      * The attributes that should be cast.
      *
