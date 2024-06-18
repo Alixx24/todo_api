10007f7cf073bdaf1a40b1a22f35d8699bcef493
Ali Mohammadi <154596621+Alixx24@users.noreply.github.com>
2024-06-19 01:54:39 +0330

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Task\CategoryController;
 use App\Http\Controllers\Task\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
